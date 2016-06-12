d9c9035a1db979c614595dd70f93d964dd4bfa36
Niklas Ulvinge <ulvinge@gmail.com>
2016-06-12 13:20:46 +0200

    Tests for lastSuccessfulLoginDate

--- risk_tests.py
+++ risk_tests.py
@@ -12,16 +12,16 @@ class RiskTestCase(unittest.TestCase):
         pass
 
 
-    def successfull(self, username="test", ip="123.123.123.123"):
+    def successfull(self, username="test", ip="123.123.123.123", time="20140616 09:02:46"):
         return self.app.post('/log', data=dict(
-            msg="20140616 09:08:11 vm5 [4f8a7f94:533e22a7] sshd Accepted password for %s from %s port 57912 ssh2"
-                % (username, ip)
+            msg="%s vm5 [4f8a7f94:533e22a7] sshd Accepted password for %s from %s port 57912 ssh2"
+                % (time, username, ip)
         ), follow_redirects=True)
 
-    def failed(self, username="test", ip="123.123.123.123"):
+    def failed(self, username="test", ip="123.123.123.123", time="20140616 09:02:46"):
         return self.app.post('/log', data=dict(
-            msg="20140616 09:02:46 vm5 [4f8a7f94:533e229c] sshd Failed none for invalid user %s from %s port 52753 ssh2"
-                % (username, ip)
+            msg="%s vm5 [4f8a7f94:533e229c] sshd Failed none for invalid user %s from %s port 52753 ssh2"
+                % (time, username, ip)
         ), follow_redirects=True)
 
     def isuserknown_fail(self):
@@ -38,6 +38,9 @@ class RiskTestCase(unittest.TestCase):
 
     def isipinternal(self, ip):
         return self.app.get('/isipinternal?ip=' + ip, follow_redirects=True)
+
+    def lastSuccessfulLoginDate(self, username):
+        return self.app.get('/lastSuccessfulLoginDate?username=' + username, follow_redirects=True)
 
     def test_isuserknown(self):
         rv = self.isuserknown_fail()
@@ -87,6 +90,30 @@ class RiskTestCase(unittest.TestCase):
         assert 'true' in rv.data
         rv = self.isipinternal(external)
         assert 'false' in rv.data
+
+    def test_lastSuccessfulLoginDate(self):
+        time1 = "20140618 09:02:46"
+        time2 = "20140619 09:02:46"
+        time3 = "20140620 09:02:46"
+        user = 'timetest'
+        rv = self.lastSuccessfulLoginDate('')
+        assert rv.status_code == 400
+
+        rv = self.failed(user, time=time1)
+        rv = self.lastSuccessfulLoginDate(user)
+        assert 'No login' in rv.data
+        rv = self.successfull(user, time=time1)
+        rv = self.lastSuccessfulLoginDate(user)
+        assert time1 in rv.data
+        rv = self.failed(user, time=time2)
+        rv = self.lastSuccessfulLoginDate(user)
+        assert time1 in rv.data
+        rv = self.successfull(user, time=time2)
+        rv = self.lastSuccessfulLoginDate(user)
+        assert time2 in rv.data
+        rv = self.successfull(user, time=time3)
+        rv = self.lastSuccessfulLoginDate(user)
+        assert time3 in rv.data
 
 if __name__ == '__main__':
     unittest.main()
